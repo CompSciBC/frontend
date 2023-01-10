a62c82bdd23eaf1bb024b411c20cde3f80de1150
prado <pradodje@gmail.com>
2023-01-10 12:50:34 -0800

    Create global theme

---
+++ src/components/utils/styles.tsx
@@ -0,0 +1,25 @@
+export const theme = {
+  font: {
+    // font: 'font-style font-variant font-weight font-size/line-height font-family'
+    displayXL: 'font: normal normal 700 30px/40px Roboto, sans-serif;',
+    displayLarge: 'font: normal normal 700 24px/28px Roboto, sans-serif;',
+    displayMedium: 'font: normal normal 400 21px/27px Roboto, sans-serif;',
+    displaySmall: 'font: normal normal 300 18px/24px Roboto, sans-serif;',
+    heading: 'font: normal normal 700 20px/26px Roboto, sans-serif;',
+    subHeading: 'font: normal normal 700 14px/16px Roboto, sans-serif;',
+    body: 'font: normal normal 400 16px/20px "Reem Kufi", sans-serif;',
+    button: 'font: normal normal 500 16px/20px "Reem Kufi", sans-serif;',
+    caption: 'font: normal normal 400 13px/20px "Reem Kufi", sans-serif;',
+    bodyLink:
+      'font: normal normal 400 16px/20px "Reem Kufi", sans-serif; text-decoration: underline;',
+    captionLink:
+      'font: normal normal 400 13px/20px "Reem Kufi", sans-serif; text-decoration: underline;'
+  },
+  color: {
+    black: '#000000',
+    white: '#ffffff'
+  },
+  screen: {
+    small: '@media only screen and (max-width: 700px)'
+  }
+};
