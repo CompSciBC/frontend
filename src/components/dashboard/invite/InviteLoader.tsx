1d0cd39c1a9dbc2aa7b3ea5a869d8ff3900ada70
Matthew Ryan Granger <mattg1827@gmail.com>
2023-02-08 13:25:11 -0800

    Test-res-1

--- src/components/dashboard/invite/InviteLoader.tsx
+++ src/components/dashboard/invite/InviteLoader.tsx
@@ -4,7 +4,7 @@
  * @returns A URL to access an invite QR code
  */
 export default async function InviteLoader(): Promise<string> {
-  const resId = 'test-res'; // TODO: replace hard coded resId
+  const resId = 'test-res-1'; // TODO: replace hard coded resId
   const response = await fetch(`/api/invites/${resId}/qr-code`);
   const body = await response.json();
   return body.data[0];
