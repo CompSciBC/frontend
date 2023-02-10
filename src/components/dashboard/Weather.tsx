7cf17fd4218a95ad059e138893d13f827a0df117
Hieu Do <hieu.do@bellevuecollege.edu>
2023-02-09 19:15:06 -0800

    lock weather route for unauthenticated users

--- src/components/dashboard/Weather.tsx
+++ src/components/dashboard/Weather.tsx
@@ -1,4 +1,9 @@
+import { Navigate } from 'react-router-dom';
+
 function Weather() {
+  if (localStorage.getItem('username') === null) {    
+    return <Navigate replace to="/" />;
+  };
   return <>Weather</>;
 }
 
