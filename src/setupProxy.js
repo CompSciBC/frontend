63f3ec1e4cfeb787185504d0a8d5db19c74b3300
Hieu Do <hieu.do@bellevuecollege.edu>
2023-02-24 19:09:56 -0800

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,14 +2,20 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  let url = 'http://localhost:8080';
   if (process.env.REACT_APP_BUILD_ENV === 'prod') {
-    url = 'http://bmgalb-1523887164.us-west-2.elb.amazonaws.com:8080';
+    app.use(
+      createProxyMiddleware('/api', {
+        target: 'http://bmgalb-1523887164.us-west-2.elb.amazonaws.com:8080',
+        changeOrigin: true
+      })
+    );
+  } else {
+    app.use(
+      createProxyMiddleware('/api', {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      })
+    );
   }
-  app.use(
-    createProxyMiddleware('/api', {
-      target: url,
-      changeOrigin: true
-    })
-  );
+  
 };
