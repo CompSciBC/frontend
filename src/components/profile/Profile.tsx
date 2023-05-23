e1e52e12ae221534a2cfc5a421be7cbd950f5042
Elena Ochkina <elena.ochkina31@gmail.com>
2023-05-22 22:32:36 -0700

    Commented out Theme in Profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -17,13 +17,13 @@ import EditIcon from '@mui/icons-material/Edit';
 import SaveIcon from '@mui/icons-material/Save';
 import { server } from '../../index';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary
+// }));
 
 const month = [
   'January',
