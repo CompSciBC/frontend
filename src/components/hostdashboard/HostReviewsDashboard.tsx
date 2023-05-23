dfdafa4a304d235820072b86d895c7a6d9a525b0
Hieu Do <hieu.do@bellevuecollege.edu>
2023-05-22 22:28:40 -0700

    reviews table wip

--- src/components/hostdashboard/HostReviewsDashboard.tsx
+++ src/components/hostdashboard/HostReviewsDashboard.tsx
@@ -1,114 +1,90 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import '@aws-amplify/ui-react/styles.css';
 import styled from '@emotion/styled';
 import { theme } from '../../utils/styles';
-import ReservationCard from './ReservationCard';
-import ReviewCard from './ReviewCard';
-import GanttChart from './GanttChart';
 import AppContext from '../../context/AppContext';
 import { useContext, useState, useEffect, useRef } from 'react';
 import { HostProvider, HostContextType } from './hostContext';
 import { server } from '../../index';
 import { Reservation, SurveyData } from '../../utils/dtos';
 import surveysJson from './mock_data_delete_later/surveys.json';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { Container, Grid, Box } from '@mui/material';
+
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params: GridValueGetterParams) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 
 function HostReviewsDashboard() {
   const { user } = useContext(AppContext);
-  // const reservations = reservationsJson.data;
-  // const surveys = JSON.stringify(surveysJson);
-  // const reviews: SurveyMetrics = JSON.parse(surveys);
-
-  // console.log('reservations');
-  // console.log(JSON.stringify(reservationsJson));
-  // console.log('reviews');
-  // console.log(JSON.stringify(reviews));
   const [reservationButton, setReservationButton] =
     useState<string>('getCurrent');
   const [reservations, setReservations] = useState<Reservation[]>([]);
-//   console.log(surveysJson.surveyResponses);
-//   const [reviews, setReviews] = useState<SurveyData[]>(surveysJson.surveyResponses);
-//   setReviews(surveysJson);
-//   useEffect(() => {
-//     fetch(`${server}/api/surveys/hostmetrics?id=${user!.userId}`)
-//       .then(async (res) => {
-//         return await res.json();
-//       })
-//       .then((data) => {
-//         setReviews(data);
-//       });
-//   }, []);
     const reviews = surveysJson;
 //   console.log('reviews');
     console.log(reviews.surveyResponses[0]);
-    function createData(
-        name: string,
-        calories: number,
-        fat: number,
-        carbs: number,
-        protein: number,
-    ) {
-        return { name, calories, fat, carbs, protein };
-    }
-    
-    const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ];
 
   const [host, setHost] = useState<HostContextType>({
     reservations,
     reviews
   });
   
-  // console.log('reservations');
-  // console.log(reservations);
-  // console.log('host');
-  // console.log(host);
   return (
     <HostProvider value={host}>
-      <Container>
-        <WidgetTitle>
-          <h3 style={{ float: 'left' }}> Reviews </h3>
-        </WidgetTitle>
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {rows.map((row) => (
-                    <TableRow
-                    key={row.name}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                    <TableCell component="th" scope="row">
-                        {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+      <Container maxWidth="md">
+        <Box sx={{ mt: 5 }}>
+            <Grid container wrap="nowrap" spacing={2}>
+                <Grid item xs={12}>
+                    <WidgetTitle>
+                        <h3 style={{ float: 'left' }}> Your Week at a Glance </h3>
+                    </WidgetTitle>
+                </Grid>
+            </Grid>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                pagination: {
+                    paginationModel: { page: 0, pageSize: 5 },
+                },
+                }}
+                pageSizeOptions={[5, 10]}
+                // checkboxSelection
+                sx={{ m: 2 }}
+            />
+        </Box>
       </Container>
     </HostProvider>
   );
@@ -190,20 +166,5 @@ const WidgetTitle = styled.div`
     text-decoration: underline;
     color: black;
     ${theme.font.body}
-  }
-`;
-
-const Container = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 85%;
-  gap: 16px;
-  padding: 32px 0;
-
-  ${theme.screen.small} {
-    width: 100%;
-    padding: 16px;
   }
 `;
