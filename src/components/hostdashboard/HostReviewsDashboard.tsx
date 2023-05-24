ca0ec83c598ed126fe0a3fcec6d4dc53dbe468a0
Hieu Do <hieu.do@bellevuecollege.edu>
2023-05-23 18:14:58 -0700

    wip

--- src/components/hostdashboard/HostReviewsDashboard.tsx
+++ src/components/hostdashboard/HostReviewsDashboard.tsx
@@ -7,53 +7,92 @@ import AppContext from '../../context/AppContext';
 import { useContext, useState, useEffect, useRef } from 'react';
 import { HostProvider, HostContextType } from './hostContext';
 import { server } from '../../index';
-import { Reservation, SurveyData } from '../../utils/dtos';
+import { Reservation, SurveyData, SurveyMetrics, User } from '../../utils/dtos';
 import surveysJson from './mock_data_delete_later/surveys.json';
 import * as React from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { Container, Grid, Box } from '@mui/material';
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+import { Container, Grid, Box, Button, Avatar } from '@mui/material';
 
 const columns: GridColDef[] = [
-  { field: 'reservationId', headerName: 'ID', width: 70 },
-  { field: 'submissionTime', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`
-  }
+  { field: 'id', headerName: 'ID', width: 50 },
+  { field: 'property', headerName: 'Property', width: 200 },
+  { field: 'guest', headerName: 'Guest Name', width: 100,
+    renderCell: (params: GridRenderCellParams<User>) => (
+        <strong>
+            {/* {params.value} */}
+            <Avatar 
+                sx={{ bgcolor: theme.color.purple, width: 40, height: 40  }}
+            >
+                {params.value.firstName.charAt(0)}{params.value.lastName.charAt(0)}
+            </Avatar>
+            {/* <Button
+                variant="contained"
+                size="small"
+                style={{ marginLeft: 16 }}
+                tabIndex={params.hasFocus ? 0 : -1}
+            >
+                {params.value.firstName.charAt(0)}{params.value.lastName.charAt(0)}
+            </Button> */}
+        </strong>
+    ),
+    },
+  { field: 'timestamp', headerName: 'Submission Time', width: 150 },
+//   {
+//     field: 'age',
+//     headerName: 'Age',
+//     type: 'number',
+//     width: 90
+//   },
+//   {
+//     field: 'fullName',
+//     headerName: 'Full name',
+//     description: 'This column has a value getter and is not sortable.',
+//     sortable: false,
+//     width: 160,
+//     valueGetter: (params: GridValueGetterParams) =>
+//       `${params.row.firstName || ''} ${params.row.lastName || ''}`
+//   }
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
-];
+// const rows = [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
+// ];
+
+
+function createRows(surveyData: SurveyData[]) {
+    const rows = [];
+
+    for (let i: number = 0; i < surveyData.length; i++) {
+        const s = surveyData[i];
+        rows.push({
+            id: i,
+            reservation: s.reservationId,
+            property: s.property.name,
+            guest: s.guest,
+            timestamp: `${s.submissionTime}`
+        });
+      }
+    return rows;
+};
 
 function HostReviewsDashboard() {
   const { user } = useContext(AppContext);
   const [reservationButton, setReservationButton] =
     useState<string>('getCurrent');
   const [reservations, setReservations] = useState<Reservation[]>([]);
-  const reviews = surveysJson;
-  //   console.log('reviews');
-  console.log(reviews.surveyResponses[0]);
+  const reviews: SurveyMetrics = JSON.parse(JSON.stringify(surveysJson));
+  const surveyResponses = reviews.surveyResponses;
+//   console.log(surveyResponses);
+    const rows = createRows(surveyResponses);
+    // console.log(rows);
 
   const [host, setHost] = useState<HostContextType>({
     reservations,
@@ -62,7 +101,7 @@ function HostReviewsDashboard() {
 
   return (
     <HostProvider value={host}>
-      <Container maxWidth="md">
+      <Container maxWidth="lg">
         <Box sx={{ mt: 5 }}>
           <Grid container wrap="nowrap" spacing={2}>
             <Grid item xs={12}>
@@ -72,16 +111,22 @@ function HostReviewsDashboard() {
             </Grid>
           </Grid>
           <DataGrid
-            rows={surveysJson.surveyResponses}
+            rows={rows}
             columns={columns}
             initialState={{
               pagination: {
                 paginationModel: { page: 0, pageSize: 5 }
               }
             }}
-            pageSizeOptions={[5, 10]}
+            pageSizeOptions={[5, 10, 15, 20]}
             // checkboxSelection
-            sx={{ m: 2 }}
+            sx={{ 
+                m: 2,
+                '&.MuiDataGrid-root--densityComfortable .MuiDataGrid-cell': { py: '22px' },
+            }}
+            getRowHeight={() => 'auto'}
+            getEstimatedRowHeight={() => 200}
+            density='comfortable'
           />
         </Box>
       </Container>
