f1f530d9912f8bcae338a86fb36077fba381ceba
Hieu Do <hieu.do@bellevuecollege.edu>
2023-05-31 11:51:10 -0700

    direct guests to reservations page on login

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,6 @@ import ForgotPassword from './components/auth/ForgotPassword';
 import Logout from './components/auth/Logout';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import HostLanding from './components/hostdashboard/HostLanding';
-import GuestLanding from './components/home/GuestLanding';
 import Profile from './components/profile/Profile';
 import Reservations from './components/reservations/Reservations';
 import AddReservation from './components/reservations/AddReservation';
@@ -60,7 +59,6 @@ export const routes = {
   forgotPassword: '/forgotPassword',
   logout: '/logout',
   hostLanding: '/hostLanding',
-  guestLanding: '/guestLanding',
   profile: '/profile',
   reservations: '/reservations',
   addReservation: '/reservations/add/:resId',
@@ -195,10 +193,6 @@ const router = createBrowserRouter([
           {
             path: routes.hostLanding,
             element: <HostLanding />
-          },
-          {
-            path: routes.guestLanding,
-            element: <GuestLanding />
           },
           {
             path: routes.profile,
